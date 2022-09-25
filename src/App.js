25882aaf6bc22f9e96cd855c05157e2f095fa89f
reginavic <newmail.0518@gmail.com>
2022-09-25 14:51:06 +0300

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import {data} from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [flower, setFlower] =  useState(0);
+
+    const [showText, setShowText] =useState(false);
+
+    const {id, name, description, species, image, showMore} = data[flower];
+
+    const previouseFlower = () => {
+        setFlower ((flower => {
+            flower --;
+            if (flower < 0) {return data.length -1;}
+            return flower;
+        }))
+    }
+    const nextFlower = () => {
+        setFlower ((flower => {
+                flower ++;
+                if (flower > data.length -1) {flower=0;}
+                return flower;
+        }))
+    }
+
+    const showTextClick = (flower) => {
+        flower.showMore= !flower.showMore
+        setShowText(!showText)
+    }
+
+    return (
+        <div>
+            <div className='container'>
+                <h1>Top 5 beautiful flowers</h1>
+            </div>
+            <div className='container'> 
+            < div className="block">
+                <img src={image} width='400px' alt='plant' className="pic" />   
+                <div className='container'>  
+                <button className="btn" onClick={previouseFlower}>Previous</button>
+                <button className="btn" onClick={nextFlower}>Next</button>  
+                </div>
+            </div>
+            </div>
+            <div className='container'>
+                <h2>{id} - {name}</h2>
+            </div>
+            <div className='container'>
+                <h3>Species: {species}</h3>
+            </div>
+            <div className='container'>
+                <p>{showMore ? description : description.substring(0,200) + '...'}
+                <button className="show" onClick={() => showTextClick(data[flower])}> {showMore ? 'show less' : 'show more'} </button>
+                </p>
+            </div>
+        </div>
+        
+    )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,83 @@
+export const data = [
+
+    {
+  
+      id: 1,
+  
+      name: "Lavanda",
+  
+      description: "Lavandula (common name lavender) is a genus of 47 known species of flowering plants in the mint family, Lamiaceae. It is native to the Old World  and is found in Cape Verde and the Canary Islands, and from Europe across to northern and eastern Africa, the Mediterranean, southwest Asia to India. Many members of the genus are cultivated extensively in temperate climates as ornamental plants for garden and landscape use, for use as culinary herbs, and also commercially for the extraction of essential oils. The most widely cultivated species, Lavandula angustifolia, is often referred to as lavender, and there is a color named for the shade of the flowers of this species. Lavender has been used over centuries in traditional medicine and cosmetics.",
+  
+      species: 47,
+  
+      image: "https://images.unsplash.com/photo-1595562161314-4ed3f3503826?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" ,
+
+      showMore: false
+  
+    },
+  
+    {
+  
+      id: 2,
+  
+      name: "Wisteria",
+  
+      description: "Wisteria is a genus of flowering plants in the legume family, Fabaceae (Leguminosae), that includes ten species of woody twining vines that are native to China, Japan, Korea, Vietnam, Southern Canada, the Eastern United States, and north of Iran. They were later introduced to France, Germany and various other countries in Europe. Some species are popular ornamental plants. The aquatic flowering plant commonly called wisteria or 'water wisteria' is in fact Hygrophila difformis, in the family Acanthaceae.",
+  
+      species: 9,
+  
+      image: "https://images.unsplash.com/photo-1635659746630-d2f77534c104?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" ,
+
+      showMore: false
+  
+    },
+  
+    {
+  
+      id: 3,
+  
+      name: "Magnolia",
+  
+      description: "Magnolia is a large genus in the subfamily Magnolioideae of the family Magnoliaceae. It is named after French botanist Pierre Magnol.Magnolia is an ancient genus. Appearing before bees evolved, the flowers are theorized to have evolved to encourage pollination by beetles. To avoid damage from pollinating beetles, the carpels of Magnolia flowers are extremely tough. Fossilized specimens of M. acuminata have been found dating to 20 million years ago, and fossils of plants identifiably belonging to the Magnoliaceae date to 95 million years ago. Another aspect of Magnolia considered to represent an ancestral state is that the flower bud is enclosed in a bract rather than in sepals; the perianth parts are undifferentiated and called tepals rather than distinct sepals and petals. Magnolia shares the tepal characteristic with several other flowering plants near the base of the flowering plant lineage such as Amborella and Nymphaea (as well as with many more recently derived plants such as Lilium).The natural range of Magnolia species is a disjunct distribution, with a main center in east and southeast Asia and a secondary center in eastern North America, Central America, the West Indies, and some species in South America.",
+  
+      species: 240,
+  
+      image: "https://images.unsplash.com/photo-1620124646959-00ee123dbe11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fG1hZ25vbGlhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" ,
+
+      showMore: false
+  
+    },
+  
+    {
+  
+      id: 4,
+  
+      name: "Peony",
+  
+      description: "The peony or paeony is a flowering plant in the genus Paeonia, the only genus in the family Paeoniaceae. Peonies are native to Asia, Europe and Western North America. Scientists differ on the number of species that can be distinguished, ranging from 25 to 40, although the current consensus is 33 known species. They have compound, deeply lobed leaves and large, often fragrant flowers, in colors ranging from purple and pink to red, white or yellow, in late spring and early summer. The flowers have a short blooming season, usually only 7–10 days. Peonies are popular garden plants in temperate regions. Herbaceous peonies are also sold as cut flowers on a large scale, although generally only available in late spring and early summer.",
+  
+      species: 33,
+  
+      image: "https://images.unsplash.com/photo-1626976109816-08cef8600d7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHBlb255fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" ,
+
+      showMore: false
+  
+    },
+  
+    {
+  
+      id: 5,
+  
+      name: "Hydrangea",
+  
+      description: "Hydrangea, common names hydrangea or hortensia, is a genus of over 75 species of flowering plants native to Asia and the Americas. By far the greatest species diversity is in eastern Asia, notably China, Korea, and Japan. They can be either deciduous or evergreen, though the widely cultivated temperate species are all deciduous. The earlier name, Hortensia, is a Latinised version of the French given name Hortense, honoring French astronomer and mathematician Nicole-Reine Hortense Lepaute.",
+  
+      species: 75,
+  
+      image: "https://images.unsplash.com/photo-1594676979216-380a0257305d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNzU1NXwwfDF8c2VhcmNofDZ8fGh5ZHJhbmdlYXxlbnwwfHx8fDE2NjM4MzM2OTA&ixlib=rb-1.2.1&q=80&w=400" ,
+
+      showMore: false
+  
+    }
+  
+  ]
